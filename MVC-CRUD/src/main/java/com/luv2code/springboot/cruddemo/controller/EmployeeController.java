a378e3157a38fd56bdbe9ab127b40dfd1602c5a1
ChandrasekarPerumal <chandruselv@gmail.com>
2023-11-22 12:36:54 +0530

    Add new employee form and redirect to list of employees

--- MVC-CRUD/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
+++ MVC-CRUD/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
@@ -20,11 +22,7 @@ public class EmployeeController {
 		this.employeeService = theEmployeeService;
 	}
 	
-	
-	public String hello() {
-		return "Hello";
-	}
-	
+		
 	@GetMapping("/list")
 	public String listEmployees(Model theModel) {
 				
@@ -36,8 +34,29 @@ public class EmployeeController {
 		// add to the spring model
 		theModel.addAttribute("employees",theEmployees);
 		
-		return "list-employees";
+		return "employees/list-employees";
 	}
 	
-
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		Employee theEmployee = new Employee();
+		
+		theModel.addAttribute("employee",theEmployee);
+		
+		return "employees/employee-form";
+	}
+	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+		
+		employeeService.save(theEmployee);
+		
+		// Use redirect to prevent the duplicate submissions
+		// So pattern used here is POST/Redirect/GET
+		return "redirect:list";
+	}
+	
+	
 }
